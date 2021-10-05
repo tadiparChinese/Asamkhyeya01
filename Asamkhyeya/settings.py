5fd6bcefa9a6c8d16baea0a645ccc8a83be80934
Pratik Nagdeve <11483594+tadiparChinese@users.noreply.github.com>
2021-10-05 14:26:39 +0530

    Debug = False

--- Asamkhyeya/settings.py
+++ Asamkhyeya/settings.py
@@ -24,7 +24,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = '5o)5cgi62(z+=u$%m6-#d02nar7xf=5fml6!1&l(ue0imbrnd%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
